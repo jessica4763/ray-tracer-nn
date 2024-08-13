2d1c3fd3280768c2654e97aa01ee56d20df3d648
Amoriod <43970431+Amoriod@users.noreply.github.com>
2024-08-14 01:58:54 +1200

    Added basic CNN

--- neural_network.py
+++ neural_network.py
@@ -4,9 +4,10 @@ import os
 import sys
 import torch
 from torch import nn
+import torch.nn.functional as F
 from torch.utils.data import DataLoader
 from torchvision import datasets, transforms
-from process_data import training_images, validation_images, training_image_classifications, validation_image_classifications, training_data_path, validation_data_path
+from process_data import training_image_names, validation_image_names, training_classifications, validation_classifications, training_data_path, validation_data_path
 
 device = (
     "cuda"
@@ -30,26 +31,33 @@ labels_map = {
     9: "Did not nudge shadow ray.",
 }
 
-num_training_images = 3000
-num_validation_images = 300
+num_training_images = 1000
+num_validation_images = 100
 
 image_height = 225
 image_width = 400
 
+network_type = "CNN"
+cost_function = "quadratic"
+
 num_input_neurons = image_height * image_width
 num_output_neurons = len(labels_map)
 mini_batch_size = 10
-num_epochs = 30
-learning_rate = 0.1
+num_epochs = 100
+learning_rate = 0.01
+
+regularizer = "L2"
+regularization_parameter = 1 / mini_batch_size
+
 
 class NeuralNetwork(nn.Module):
     def __init__(self):
         super().__init__()
         self.flatten = nn.Flatten()
         self.linear_sigmoid_stack = nn.Sequential(
-            nn.Linear(image_height * image_width, 500),
-            nn.ReLU(),
-            nn.Linear(500, num_output_neurons),
+            nn.Linear(image_height * image_width, 1000),
+            nn.Sigmoid(),
+            nn.Linear(1000, num_output_neurons),
             nn.Sigmoid(),
         )
 
@@ -57,11 +65,75 @@ class NeuralNetwork(nn.Module):
         x = self.flatten(x)
         logits = self.linear_sigmoid_stack(x)
         return logits
+
+
+class ConvolutionalNeuralNetwork(torch.nn.Module):
+    def __init__(self):
+        super(ConvolutionalNeuralNetwork, self).__init__()
+        # 1 input image channel (black & white), 6 output channels, 6 x 6 square convolution kernel
+        # Apply 5 x 5 max-pooling after the convolutional layer
+        self.conv1 = torch.nn.Conv2d(1, 6, 6)
+        # 6 input image channels, 16 output channels, 5 x 5 square convolutional layer
+        # Apply 5 x 5 max-pooling after the convolutional layer
+        self.conv2 = torch.nn.Conv2d(6, 16, 5)
+        # an affine operation: y = Wx + b
+        self.fc1 = torch.nn.Linear(16 * 15 * 8, 120)  # 15 * 8 from resulting image dimension after both convolutional and max-pooling layers
+        self.fc2 = torch.nn.Linear(120, 84)
+        self.fc3 = torch.nn.Linear(84, 10)
+
+    def forward(self, x):
+        # Max pooling over a (5, 5) window
+        x = F.max_pool2d(F.relu(self.conv1(x)), (5, 5))
+        # If the size is a square you can only specify a single number
+        x = F.max_pool2d(F.relu(self.conv2(x)), 5)
+        x = x.view(-1, self.num_flat_features(x))
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
     
-model = NeuralNetwork().to(device)
+    def num_flat_features(self, x):
+        size = x.size()[1:]  # all dimensions except the batch dimension
+        num_features = 1
+        for s in size:
+            num_features *= s
+        return num_features
+    
+
+# Load images and their classifications into memory 
+def load_images_and_classifications(data_path, image_names, num_images, image_classifications):
+    loaded_images = torch.empty(0, image_height, image_width, dtype=torch.float32).to(device)
+    loaded_classifications = torch.empty(0, num_output_neurons).to(device)
+
+    for image_index in range(num_images):
+        image_name = image_names[image_index]
+        image_path = os.path.join(data_path, image_name)
+
+        sample_image = imread(image_path, format="ppm")
+        sample_image = sample_image[:,:,0]
+        sample_image = (sample_image / 255).astype('float32')
+        sample_image = torch.from_numpy(sample_image).unsqueeze(0).to(device)
+        loaded_images = torch.cat((loaded_images, sample_image), dim=0)
+
+        sample_classification = image_classifications[image_name]
+        sample_classification = torch.tensor(sample_classification).unsqueeze(0).to(device)
+        loaded_classifications = torch.cat((loaded_classifications, sample_classification), dim=0)
+
+        if image_index % 10 == 0:
+            print(f"{image_index} images loaded")
+
+    return loaded_images, loaded_classifications
+
+
+loaded_training_images, loaded_training_classifications = load_images_and_classifications(training_data_path, training_image_names, num_training_images, training_classifications)
+loaded_validation_images, loaded_validation_classifications = load_images_and_classifications(validation_data_path, validation_image_names, num_validation_images, validation_classifications)
+model = ConvolutionalNeuralNetwork().to(device) if network_type == "CNN" else NeuralNetwork().to(device)
+
+# Define cross-entropy cost function
+binary_cross_entropy_cost = nn.BCELoss()
 
 # Use a basic stochastic gradient descent optimizer
-optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
+optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate, weight_decay=regularization_parameter if regularizer == "L2" else 0)
 
 with open("../data/log.txt", 'w') as file:
     # Train for some number of epochs
@@ -69,33 +141,23 @@ with open("../data/log.txt", 'w') as file:
         for batch_start_index in range(0, num_training_images, mini_batch_size):
             print(f"epoch: {epoch} | batch_start_index: {batch_start_index}")
             
-            mini_batch = torch.empty(0, image_height, image_width, dtype=torch.float32).to(device)
-            mini_batch_classifications = torch.empty(0, num_output_neurons).to(device)
-            for training_image_index in range(batch_start_index, batch_start_index + mini_batch_size):
-                training_image_name = training_images[training_image_index]
-                training_image_path = os.path.join(training_data_path, training_image_name)
-
-                sample_training_image = imread(training_image_path, format="ppm")
-                sample_training_image = sample_training_image[:,:,0]
-                sample_training_image = (sample_training_image / 255).astype('float32')
-                sample_training_image = torch.from_numpy(sample_training_image).unsqueeze(0).to(device)
-                mini_batch = torch.cat((mini_batch, sample_training_image), dim=0)
-
-                mini_batch_classification = training_image_classifications[training_image_name]
-                mini_batch_classification = torch.tensor(mini_batch_classification).unsqueeze(0).to(device)
-                mini_batch_classifications = torch.cat((mini_batch_classifications, mini_batch_classification), dim=0)
-
+            mini_batch = loaded_training_images[batch_start_index:batch_start_index + mini_batch_size, :, :].unsqueeze(1)  # .unsqueeze(1) when using the CNN
+            mini_batch_classifications = loaded_training_classifications[batch_start_index:batch_start_index + mini_batch_size, :]
             mini_batch.requires_grad_(False)
-
+            
             # A 2-dimensional output. Each row is the predicted classification for a particular training image in the mini-batch
             training_prediction = model(mini_batch)
 
-            # Define the quadratic cost function
-            quadratic_cost = (((mini_batch_classifications - training_prediction) ** 2) / (2 * mini_batch_size)).sum()
-            print(f"Quadratic cost: {quadratic_cost}")
+            # Define the cost function
+            if cost_function == "quadratic":
+                cost = (((mini_batch_classifications - training_prediction) ** 2) / (2 * mini_batch_size)).sum()
+            elif cost_function == "binary cross entropy":
+                cost = binary_cross_entropy_cost(training_prediction, mini_batch_classifications)
+
+            print(f"Cost: {cost}")
 
             # Backpropagation
-            quadratic_cost.backward()
+            cost.backward()
 
             # Update weights and biases 
             optimizer.step()
@@ -106,29 +168,19 @@ with open("../data/log.txt", 'w') as file:
         # Check the classifier accuracy against the validation data every epoch
         correct = 0
         for validation_image_index in range(num_validation_images):
-            validation_image_name = validation_images[validation_image_index]
-            validation_image_path = os.path.join(validation_data_path, validation_image_name)
-
-            sample_validation_image = imread(validation_image_path, format="ppm")
-            sample_validation_image = sample_validation_image[:,:,0]
-            sample_validation_image = (sample_validation_image / 255).astype('float32')
-            # We have a batch size of 1 in this case. We want to add an extra dimension to the tensor, 
-            # like this: (H, W) -> (1, H, W) - hence .unsqueeze(0)
-            sample_validation_image = torch.from_numpy(sample_validation_image).unsqueeze(0).to(device)
-
-            sample_validation_classification = validation_image_classifications[validation_image_name]
-            sample_validation_classification = torch.tensor(sample_validation_classification).to(device)
+            sample_validation_image = loaded_validation_images[validation_image_index, :, :].unsqueeze(0).unsqueeze(0) # .unsqueeze(0).unsqueeze(0) when using the CNN
+            sample_validation_classification = loaded_validation_classifications[validation_image_index, :]
 
             # A 2-D tensor with size (1, num_output_neurons)
             validation_prediction = model(sample_validation_image)
 
-            threshold = 0.5 
+            # The prediction is correct if all predicted labels are within 0.5 of the actual labels 
             difference = torch.abs(sample_validation_classification - validation_prediction)
-            result = torch.all(difference < threshold).item()  # Returns a bool
+            result = torch.all(difference < 0.5).item()  # Returns a bool
             correct += int(result)
 
         validation_accuracy = correct / num_validation_images
         print(f"Validation accuracy after epoch {epoch}: {validation_accuracy} ")
 
-        file.write(f"{epoch}: {validation_accuracy:.4f} {quadratic_cost}\n")
+        file.write(f"{epoch}: {validation_accuracy:.4f} {cost}\n")
         file.flush()
--- process_data.py
+++ process_data.py
@@ -6,16 +6,14 @@ from random import choice
 import torch
 import matplotlib.pyplot as plt
 
-print(os.getcwd())
-
 training_data_path = "../data/simple_training_data_05679"
 training_data_classifications_path = "../data/simple_training_classifications_05679.csv"
 
 validation_data_path = "../data/simple_validation_data_05679"
 validation_data_classifications_path = "../data/simple_validation_classifications_05679.csv"
 
-training_images = os.listdir(training_data_path)
-validation_images = os.listdir(validation_data_path)
+training_image_names = os.listdir(training_data_path)
+validation_image_names = os.listdir(validation_data_path)
 
 
 def get_image_classifications(image_classification_path):
@@ -32,16 +30,16 @@ def get_image_classifications(image_classification_path):
 
 
 # Associate the training and validation data images with their classifications
-training_image_classifications = get_image_classifications(training_data_classifications_path)
-validation_image_classifications = get_image_classifications(validation_data_classifications_path)
+training_classifications = get_image_classifications(training_data_classifications_path)
+validation_classifications = get_image_classifications(validation_data_classifications_path)
 
 # Take a random sample of training data images and display them with their name and classification
 # figure = plt.figure(figsize=(8, 8))
 # cols, rows = 3, 3
 # for i in range(1, cols * rows + 1):
-#     sample_training_image_name = choice(training_images)
+#     sample_training_image_name = choice(training_image_names)
 #     sample_training_image_path = os.path.join(training_data_path, sample_training_image_name)
-#     sample_training_image_classification = training_image_classifications[sample_training_image_name]
+#     sample_training_image_classification = training_classifications[sample_training_image_name]
 #     sample_training_image = imread(sample_training_image_path, format="ppm")
 #     figure.add_subplot(rows, cols, i)
 #     plt.title(f"{sample_training_image_name}: {sample_training_image_classification}", fontsize=8)
